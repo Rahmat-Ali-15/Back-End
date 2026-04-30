e44b544b35b5ae86c04120c8e5e1c34ce47cad10
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-04-30 16:49:06 +0530

    Code of the day

--- Node_JS/Day_1/os.js
+++ Node_JS/Day_1/os.js
@@ -1,4 +1,25 @@
 const os = require("os")
-// console.log("🚀 ~ os:", os.cpus());
-console.log("🚀 ~ os:", os.freemem());
-console.log("🚀 ~ os:", os.machine());
+// console.log("🚀 ~ os:", os.cpus().length);
+// console.log("🚀 ~ os:", os.machine());
+// console.log("🚀 ~ os:", os.totalmem());
+// console.log("🚀 ~ os:", os.freemem());
+// console.log("🚀 ~ os:", os.hostname());
+// console.log("🚀 ~ os:", os.uptime());
+// console.log("🚀 ~ os:", os.release());
+
+// const totalMemory = os.totalmem();
+// const totalGB = (totalMemory / (1024 ** 3)).toFixed(2);
+
+// console.log("Total Memory:", totalGB, "GB");
+
+
+// const freeMemory = os.freemem();
+// const freeGB = (freeMemory / (1024 ** 3)).toFixed(2);
+
+// console.log("Free Memory:", freeGB, "GB");
+
+
+// const uptime = os.uptime();
+// const hours = (uptime / 3600).toFixed(2);
+
+// console.log("System running for:", hours, "hours");
