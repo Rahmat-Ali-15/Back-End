26be3d42154fa2b6d9631eb308d9c8b0b231a10a
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-05-12 15:13:11 +0530

    Code of the day

--- Node_JS/Day-12/routes/student.routes.js
+++ Node_JS/Day-12/routes/student.routes.js
@@ -1,33 +1,39 @@
-const express = require('express');
+const express = require("express");
 
 const studentRoutes = express.Router();
-const {studentModel} = require('../model/student.model');
+const { studentModel } = require("../model/student.model");
+const { userModel } = require("../model/user.model");
 
 // path = "/student/"
 // read
-studentRoutes.get('/', async(req, res) => {
-    const studentData = await studentModel.find();
-    res.send({msg: 'data found', data: studentData})
-})
+studentRoutes.get("/", async (req, res) => {
+  const studentData = await studentModel.find();
+  res.send({ msg: "data found", data: studentData });
+});
 
 // path = "/student/createstudent"
 // create
-studentRoutes.post('/createstudent', async (req, res) => {
-    if(req.body){
-        const studentData = new studentModel(req.body);
-        await studentData.save();
-        res.send({msg: 'data created', data: studentData})
-    }
-})
-
+studentRoutes.post("/createstudent", async (req, res) => {
+  console.log("🚀 ~ req.body:", req.body);
+  if (req.body) {
+    const userData = await new userModel.create(req.body);
+    console.log("🚀 ~ userData:", userData);
+    const studentData = new studentModel({
+      batch: req.body.batch,
+      achivements: req.body.achivements,
+      user_ID: userData._id,
+    });
+    await studentData.save();
+    res.send({ msg: "data created", data: studentData });
+  }
+});
 
 // path="/student/updatestudent"
 // update
-studentRoutes.patch('/updatestudent', async (req, res) => {
-
-})
-
+studentRoutes.patch("/updatestudent", async (req, res) => {});
 
 // path="/student/deletestudent"
 // delete
-studentRoutes.get('/deletestudent', (req, res) => {})
+studentRoutes.get("/deletestudent", (req, res) => {});
+
+module.exports = { studentRoutes };
