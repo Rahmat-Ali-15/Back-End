84d90f20be30cd3b63cd1f51d64e304f40fbb1c4
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-07-26 01:56:48 +0530

    Practice code of Express js

--- Express_Js/Express_JS_Task/HTTP_Method/server.js
+++ Express_Js/Express_JS_Task/HTTP_Method/server.js
@@ -1,82 +1,78 @@
 import express from "express";
 const app = express();
 
-const Port = 7500
+const Port = 7500;
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 // dummy database
-const students = []
+const students = [];
 
 //# GET=> Get all students
 app.get("/students", (req, res) => {
-    res.status(200).json(
-        {
-            suceess: true,
-            data: students
-        }
-    )
-})
+  res.status(200).json({
+    suceess: true,
+    data: students,
+  });
+});
 
 //# POST=> Add a new student
 app.post("/students", (req, res) => {
-    const {id,name, age} = req.body;
+  const { id, name, age } = req.body;
 
-    students.push({id, name, age})
+  students.push({ id, name, age });
 
-    res.status(201).json({
-        suceess: true,
-        msg: "Student Added Successfully",
-        data: students
-    })
-})
-
+  res.status(201).json({
+    suceess: true,
+    msg: "Student Added Successfully",
+    data: students,
+  });
+});
 
 // # PUT => Update student by ID
 app.put("/students/:id", (req, res) => {
-    const id = Number(req.params.id);
+  const id = Number(req.params.id);
 
-    const student = students.find((el) => el.id === id)
+  const student = students.find((el) => el.id === id);
 
-    if(!student){
-        return res.status(404).json({
-            suceess: false,
-            msg: "Studetn not found"
-        })
-    }
-    student.name = req.body.name;
-    student.age = req.body.age
+  if (!student) {
+    return res.status(404).json({
+      suceess: false,
+      msg: "Studetn not found",
+    });
+  }
+  student.name = req.body.name;
+  student.age = req.body.age;
 
-    res.status(200).json({
-        success: true,
-        msg: "Student updated successfully",
-        data: student,
-    })
-})
-
+  res.status(200).json({
+    success: true,
+    msg: "Student updated successfully",
+    data: student,
+  });
+});
 
 // # DELETE => Delete a student by ID
 app.delete("/students/:id", (req, res) => {
-    const  id = Number(req.params.id);
-    
-    const index = students.findIndex((el)=> el.id === id)
+  const id = Number(req.params.id);
 
-    if(index === -1){
-        return res.status(404).json({
-            success: false,
-            msg: "Student Not Found"
-        })
-    }
+  const index = students.findIndex((el) => el.id === id);
 
-    students.splice(index, 1);
-    res.status(200).json({
-        success: true,
-        msg: "Student Deleted Successfully",
-        data: students
-    })
-})
+  if (index === -1) {
+    return res.status(404).json({
+      success: false,
+      msg: "Student Not Found",
+    });
+  }
+
+  students.splice(index, 1);
+  res.status(200).json({
+    success: true,
+    msg: "Student Deleted Successfully",
+    data: students,
+  });
+});
 
 app.listen(Port, () => {
-    console.log(`Server is running on Port ${Port}`)
-})
+  console.log(`Server is running on Port ${Port}`);
+});
